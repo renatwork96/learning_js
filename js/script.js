4fa3ec858abde76f66186ba594f236cf394b3403
renatwork96 <renat.work96@gmail.com>
2021-04-07 21:24:24 +0300

    Выполнил второе домашнее задание

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-let money; 
-let income; 
-let addExpenses; 
-let deposit;
-let mission; 
-let period;
-alert('Тестовое сообщение');
-console.log('Второе тестовое сообщение');
+let money = 40000; 
+let income = 5000; 
+let addExpenses = 'Интернет, Транспорт, Комуналка'; 
+let deposit = true;
+let mission = 100000; 
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев.');
+console.log('Цель заработать ' + mission + ' рублей.');
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+let budgetDay = money / 30;
+console.log(budgetDay);
