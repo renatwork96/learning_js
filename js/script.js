b52e0f1aa430c39199192916b53d57ca3815a8be
renatwork96 <renat.work96@gmail.com>
2021-04-08 22:58:18 +0300

    Исправил 3 урок

--- js/script.js
+++ js/script.js
@@ -1,56 +1,31 @@
 'use strict';
 
-let money = 40000; 
+let money = +prompt('Ваш месячный доход?');
 let income = 5000; 
-let addExpenses = 'Интернет, Транспорт, Комуналка, Кафе'; 
-let deposit = true;
+let addExpenses =prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 100000; 
 let period = 6;
+let amount1;
+let amount2;
+let expenses1;
+let expenses2;
+let budgetDay;
+let budgetMonth;
+let missionMonth;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев.');
 console.log('Цель заработать ' + mission + ' рублей.');
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(', '));
-let budgetDay = money / 30;
-console.log(budgetDay);
+console.log(addExpenses.toLowerCase().split(', '));
 
+expenses1 = prompt('Введите обязательную статью расходов?');
+amount1 = +prompt('Во сколько это обойдется?');
+expenses2 = prompt('Введите обязательную статью расходов?');
+amount2 = +prompt('Во сколько это обойдется?');
 
-money = Number( prompt('Ваш месячный доход?') );
+budgetMonth  = money - amount1 - amount2;
 
-while (!money) {
-  alert('Вы ввели неправильное значение, повторите попытку');
-  money = Number( prompt('Ваш месячный доход?') );
-}
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let expenses1 = prompt('Введите обязательную статью расходов?');
-
-let amount1 = Number( prompt('Во сколько это обойдется?') );
-
-while (!amount1) {
-  alert('Вы ввели неправильное значение, повторите попытку');
-  amount1 = Number( prompt('Во сколько это обойдется?') );
-}
-let expenses2 = prompt('Введите обязательную статью расходов?');
-
-let amount2 =  Number( prompt('Во сколько это обойдется?') );
-
-while (!amount2) {
-  alert('Вы ввели неправильное значение, повторите попытку');
-  amount2 = Number( prompt('Во сколько это обойдется?') );
-}
-
-let budgetMonth  = money - amount1 - amount2;
-console.log(budgetMonth);
-
-let missionMonth;
+console.log('Месячный бюджет: ' + budgetMonth);
 
 if (budgetMonth > 0) {
   missionMonth = Math.ceil( mission / budgetMonth );
@@ -60,6 +35,7 @@ if (budgetMonth > 0) {
 }
 
 budgetDay = Math.floor( budgetMonth / 30 );
+
 console.log('Бюджет на день: ' + budgetDay);
 
 if (budgetDay >= 1200) {
