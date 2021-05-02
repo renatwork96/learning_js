a3323ffbb961df6b974e05a881ad6e4779f59937
renatwork96 <renat.work96@gmail.com>
2021-05-02 22:02:32 +0300

    Урок 24

--- script/script.js
+++ script/script.js
@@ -356,8 +356,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     }
     //console.log(inputsField[i]);
-    // let aaa = /.+/g;
-    // if (inputsField[i].match(aaa)) {
+    // if (/calc/.test(inputsField[i])) {
     //   console.log(inputsField[i]);
     //   inputsField[i].addEventListener('input', () => {
     //     inputsField[i].value =  inputsField[i].value.replace(/\D/g, '');
@@ -421,5 +420,60 @@ window.addEventListener('DOMContentLoaded', function() {
      }
 
   }
+
+  //Калькулятор
+
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      } 
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+
+
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      // if (target.matches('.calc-type') || target.matches('.calc-square') || target.matches('.calc-day') || target.matches('.calc-count')) {
+      //   console.log(1);
+      // }
+
+      // if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+      //   console.log(1);
+      // }
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
+  calc(100);
 
 });
