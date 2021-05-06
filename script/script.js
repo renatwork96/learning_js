877fde16d129f0589834101fc9ca1ceb9e5c80e9
renatwork96 <renat.work96@gmail.com>
2021-05-06 17:10:44 +0300

    Урок 29 с fetch

--- script/script.js
+++ script/script.js
@@ -533,37 +533,46 @@ window.addEventListener('DOMContentLoaded', function() {
         body[key] = val;
       });
 
-      const postData = (body) => {
+      const postData = (bodyForm) => {
 
-        return new Promise((resolve, reject) => {
-
-          const request = new XMLHttpRequest();
-
-          request.addEventListener('readystatechange', () => {
-
-            if (request.readyState !== 4) {
-              return;
-            }
-
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.status);
-            }
-
-          });
-
-          request.open('POST', './server.php');
-          request.setRequestHeader('Content-Type', 'application/json');
-          request.send(JSON.stringify(body));
-          
+        return fetch('./server.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(bodyForm),
+          credentials: 'include'
         });
+
+        // return new Promise((resolve, reject) => {
+        //   const request = new XMLHttpRequest();
+        //   request.addEventListener('readystatechange', () => {
+
+        //     if (request.readyState !== 4) {
+        //       return;
+        //     }
+
+        //     if (request.status === 200) {
+        //       resolve();
+        //     } else {
+        //       reject(request.status);
+        //     }
+        //   });
+
+        //   request.open('POST', './server.php');
+        //   request.setRequestHeader('Content-Type', 'application/json');
+        //   request.send(JSON.stringify(body));
+        // });
 
       };
 
     
       postData(body)
-      .then(() => {
+      .then((response) => {
+          if(response.status !== 200) {
+            throw new Error ('status network not 200');
+          }
+          console.log(response);
           statusMessage.textContent = successMessage;  
           form.reset();  
           setTimeout(() => {statusMessage.textContent = '';}, 4000);      
